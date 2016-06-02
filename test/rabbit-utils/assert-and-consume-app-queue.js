58ede68526a672cb557def6c1ddae99209426b10
Spencer Applegate <spencer@spencerapplegate.com>
2016-06-02 15:03:38 -0400

    Moved prefetch definition to app.connect

--- test/rabbit-utils/assert-and-consume-app-queue.js
+++ test/rabbit-utils/assert-and-consume-app-queue.js
@@ -2,7 +2,6 @@
 const Lab = require('lab')
 const sinon = require('sinon')
 require('sinon-as-promised')
-const Code = require('code')
 
 const Application = require('../../lib/application.js')
 const assertAndConsumeAppQueue = require('../../lib/rabbit-utils/assert-and-consume-app-queue.js')
@@ -11,7 +10,6 @@ const lab = exports.lab = Lab.script()
 const describe = lab.describe
 const it = lab.it
 const beforeEach = lab.beforeEach
-const expect = Code.expect
 
 describe('RabbitUtils - assertAndConsumeAppQueue', function () {
   let ctx
@@ -30,42 +28,6 @@ describe('RabbitUtils - assertAndConsumeAppQueue', function () {
       consume: sinon.stub().resolves()
     }
     done()
-  })
-
-  describe('provided a prefetch count in the consumeOpts', function () {
-    beforeEach(function (done) {
-      ctx = {}
-      ctx.queueName = 'queue-name'
-      ctx.queueOpts = {}
-      ctx.consumeOpts = {
-        prefetch: 1
-      }
-      ctx.app = new Application()
-      ctx.app.queue(ctx.queueName, ctx.queueOpts, ctx.consumeOpts, function * () {})
-      ctx.handler = function () {}
-      sinon.stub(ctx.app, 'messageHandler').returns(ctx.handler)
-      ctx.app.consumerChannel = {
-        assertQueue: sinon.stub().resolves(),
-        consume: sinon.stub().resolves()
-      }
-      done()
-    })
-
-    it('should set a prefetch count on the consumerChannel', function (done) {
-      assertAndConsumeAppQueue(ctx.app, ctx.queueName).then(function () {
-        expect(ctx.app.consumerChannel.prefetch).to.equal(1)
-        done()
-      }).catch(done)
-    })
-  })
-
-  describe('not provided a prefetch count in the consumeOpts', function () {
-    it('should not set a prefetch count on the consumerChannel', function (done) {
-      assertAndConsumeAppQueue(ctx.app, ctx.queueName).then(function () {
-        expect(ctx.app.consumerChannel.prefetch).to.not.equal(1)
-        done()
-      }).catch(done)
-    })
   })
 
   it('should assert and assume the queue', function (done) {
